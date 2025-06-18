f461fe9c96c0e593869463525f61bfca7e640744
Techbandit001 <davidkiptum80@gmail.com>
2025-06-18 21:02:32 +0300

    add changes

--- models.py
+++ models.py
@@ -15,9 +15,23 @@ class Hero(db.Model, SerializerMixin):
     supername = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    hero_powers = db.relationship("HeroPower", back_populates="hero", cascade="all, delete-orphan")
+
 class Power(db.Model, SerializerMixin):
     __tablename__ = "powers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    hero_powers = db.relationship("HeroPower", back_populates="power", cascade="all, delete-orphan")
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    hero = db.relationship("Hero", back_populates="hero_powers")
+    power = db.relationship("Power", back_populates="hero_powers")
